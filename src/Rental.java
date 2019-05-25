c524fa6dafed593578b5cdc08710b73d4b4216fc
Ana Flavia Marcelino <anaflaviamarcelino@gmail.com>
2019-05-24 23:09:23 -0300

    Refactoring 5: Extract Method

--- src/Rental.java
+++ src/Rental.java
@@ -33,4 +33,12 @@ class Rental {
 		}
 		return amount;
     }
+    
+    int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+		    getDaysRented() > 1) 
+			return 2;
+		else
+		return 1;
+	}
 }
