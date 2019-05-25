60fdbb372900e7b5fb068ea3edfcb7568c4a1059
Ana Flavia Marcelino <anaflaviamarcelino@gmail.com>
2019-05-24 23:35:35 -0300

    Refactoring 7: Replace Conditional with Polymorphism

--- src/Rental.java
+++ src/Rental.java
@@ -15,30 +15,11 @@ class Rental {
     }
     
     double getCharge() {
-    	double amount = 0;
-		switch (getMovie().getPriceCode()) {
-		   case Movie.REGULAR:
-		      amount += 2;
-		      if (getDaysRented() > 2)
-		         amount += (getDaysRented() - 2) * 1.5;
-		      break;
-		   case Movie.NEW_RELEASE:
-		      amount += getDaysRented() * 3;
-		      break;
-		   case Movie.CHILDRENS:
-		      amount += 1.5;
-		      if (getDaysRented() > 3)
-		         amount += (getDaysRented() - 3) * 1.5;
-		       break;
-		}
-		return amount;
+    	return _movie.getCharge(_daysRented);
     }
+	
     
     int getFrequentRenterPoints() {
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-		    getDaysRented() > 1) 
-			return 2;
-		else
-		return 1;
+		return _movie.getFrequentRenterPoints(_daysRented);
 	}
 }
